ebbbf34c3b3372c2d430b9e3865d4d7b8bedf352
dreadlus31 <dreadlus31@gmail.com>
2020-01-13 12:26:04 +0100

    Implemented functionnalities the way I thought they should work, since I already have some tests definition provided. Set balance value to int, in order to lessen problems with Double and double multiplication and division. Made static conversion function to simplify euro to cent, or more likely Double to int, conversion.

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -2,22 +2,37 @@ package com.priceminister.account.implementation;
 
 import com.priceminister.account.*;
 
-
 public class CustomerAccount implements Account {
 
+    /**
+     * @param balance stored as cents of Euros. Expressed as Euros.
+     */
+    private int balance = 0;
+
     public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
+        this.balance += new Double(addedAmount * 100).intValue();
     }
 
     public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
+        return toEuros(this.balance);
     }
 
-    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
-    		throws IllegalBalanceException {
-        // TODO Auto-generated method stub
-        return null;
+    public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) throws IllegalBalanceException {
+        int remainingBalance = balance - toCents(withdrawnAmount);
+        Double results = toEuros(remainingBalance);
+        if (rule.withdrawPermitted(results)) {
+            balance = remainingBalance;
+            return results;
+        }
+        throw new IllegalBalanceException(results);
+    }
+
+    private static int toCents(Double euros) {
+        return new Double(euros * 100).intValue();
+    }
+
+    private static Double toEuros(int cents) {
+        return (new Double(cents)) / 100;
     }
 
 }
