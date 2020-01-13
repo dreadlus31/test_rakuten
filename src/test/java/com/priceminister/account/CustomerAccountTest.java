3d726fd9547343dd90eb609ec3de150ccceff704
dreadlus31 <dreadlus31@gmail.com>
2020-01-13 13:37:02 +0100

    Renamed a test

--- src/test/java/com/priceminister/account/CustomerAccountTest.java
+++ src/test/java/com/priceminister/account/CustomerAccountTest.java
@@ -84,7 +84,7 @@ public class CustomerAccountTest {
 
     }
 
-    public void testChainDepositsAndCheckBalanceMatchWithTotal() {
+    public void testChainDepositsAndWithdrawalAndCheckBalanceMatchWithTotal() {
         Double total = 0.0;
         try {
             for (int i = 0; i < 10; i++) {
